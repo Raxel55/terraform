c83c2c6acb3609875d3b091ab40b0ed7241fa632
AUstinov <AUstinov@its.jnj.com>
2021-04-07 18:02:43 +0300

    Added WORDPRESS_CONFIG_EXTRA

--- ssm.tf
+++ ssm.tf
@@ -33,3 +33,16 @@ resource "aws_ssm_parameter" "db-host" {
   value       = aws_db_instance.kanda.address
   tags = local.common_tags
 }
+
+resource "aws_ssm_parameter" "wp-config-extra" {
+  name        = "/${local.name_prefix}/wordpress/config_extra"
+  description = "Wordpress config extra"
+  type        = "String"
+  overwrite   = true
+  value       = <<WORDPRESS_CONFIG_EXTRA
+/* Additional configuration */
+define( 'WP_SITEURL', '${var.wp-site-url}' );
+define( 'WP_HOME', '${var.wp-site-url}' );
+WORDPRESS_CONFIG_EXTRA
+  tags = local.common_tags
+}
